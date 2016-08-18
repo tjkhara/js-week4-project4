74c34a4244a8f9048f8475645db96850bfadcabc
Tajeshwar Khara <tkhara@gmail.com>
2016-08-18 14:50:36 -0700

    Question 3 was missing in last commit
    Here it is...

--- help.js
+++ help.js
@@ -2,59 +2,65 @@ $(document).ready(function(){
   $("#1").click(function(){
     $("#hidden1").css("display", "inline")
   })
-  
+
   $("#submitBtn").click(function(){
-        
-    
+
+
         var a = $("#textInput").val();
         $( "<h3>" + a + "</h3>" ).insertAfter( ".test2" );
-        
+
     });
-    
+
   $("#2").click(function(){
     $("#hidden2").css("display", "inline")
   })
-  
+
   $(".click").click(function(){
-        
+
       if($('#checkbox1').prop('checked')) {
-          
+
           $( "h2" ).removeClass("hidden");
-          
-          
+
+
           } else {
               $( "h2" ).addClass("hidden");
           }
-    
+
   });
-    
+
 //    Adding a quick comment to test git
-  
+
   $("#3").click(function(){
     $("#hidden3").css("display", "inline")
   })
-  
-  var table = $("table tbody");
 
-    table.find('tr').each(function (i) {
-        var $tds = $(this).find('td'),
-            name = $tds.eq(0).text(),
-            weight = $tds.eq(1).text(),
-            hunger = $tds.eq(2).text();
-        // do something with name, weight, hunger
-//        alert('Row ' + (i + 1) + ' :Name: ' + name
-//              + '\nWeight ' + weight
-//              + '\nHunger: ' + hunger);
-        
-        if(hunger === "Hangry") {
-            $(this).css("color", "red");
-        }
-    });
-  
-  
+$('.table > tbody:last-child').append('<tr><td>Peter</td><td>7</td><td>Hangry</td></tr>');
+
   $("#4").click(function(){
     $("#hidden4").css("display", "inline")
   })
+
+  var table = $("table tbody");
+
+  table.find('tr').each(function (i) {
+    var $tds = $(this).find('td'),
+    name = $tds.eq(0).text(),
+    weight = $tds.eq(1).text(),
+    hunger = $tds.eq(2).text();
+    // do something with name, weight, hunger
+    //        alert('Row ' + (i + 1) + ' :Name: ' + name
+    //              + '\nWeight ' + weight
+    //              + '\nHunger: ' + hunger);
+
+    if(hunger === "Hangry") {
+      $(this).css("color", "red");
+    } else if (hunger === "Content") {
+      $(this).css("color", "green");
+    }
+  });
+
+
+
   $("#5").click(function(){
     $("#hidden5").css("display", "inline")
   })
