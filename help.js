384e18585805611b03fb50dcc918b370a6fc885e
Tajeshwar Khara <tkhara@gmail.com>
2016-08-18 18:13:08 -0700

    First 7 questions attempted
    First 7

--- help.js
+++ help.js
@@ -60,16 +60,85 @@ $('.table > tbody:last-child').append('<tr><td>Peter</td><td>7</td><td>Hangry</t
   });
 
 
+  // 5. When clicked, only display cats weighing 10+ lbs:
+  //   Consider googling .each() to iterate over all of the children. Another conditional will be needed.
 
   $("#5").click(function(){
     $("#hidden5").css("display", "inline")
   })
+
+  function removeHeavy() {
+
+  var table = $("table tbody");
+
+    table.find('tr').each(function (i) {
+      var $tds = $(this).find('td'),
+      name = $tds.eq(0).text(),
+      weight = $tds.eq(1).text(),
+      hunger = $tds.eq(2).text();
+      // do something with name, weight, hunger
+      //        alert('Row ' + (i + 1) + ' :Name: ' + name
+      //              + '\nWeight ' + weight
+      //              + '\nHunger: ' + hunger);
+
+      if(weight<10) {
+        $(this).css("display", "none");
+      }
+
+    });
+
+    }
+
+    $(".btn-warning").click(
+    function() {
+      removeHeavy();
+    }
+  )
+
+
+
+
+
+
   $("#6").click(function(){
     $("#hidden6").css("display", "inline")
   })
+
+  function firstNameUpper() {
+
+  var table = $("table tbody");
+
+    table.find('tr').each(function (i) {
+      $(this).find(':first-child').css("text-transform","uppercase");
+
+    });
+
+    }
+
+    $(".btn-success").click(
+    function() {
+      firstNameUpper();
+    }
+  )
+
+
+
+
+
+
   $("#7").click(function(){
     $("#hidden7").css("display", "inline")
   })
+
+  $(".btn-primary").click(
+  function() {
+    $( ".table" ).toggle();
+  }
+  )
+
+
+
+
   $("#8").click(function(){
     $("#hidden8").css("display", "inline")
   })
